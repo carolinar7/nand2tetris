165e15c1ecc2becbffce45d0cbd1592fb6cfaeb9
Frederick Santiago <frsantiago2001@gmail.com>
2022-09-21 15:33:35 -0400

    Parser init

--- projects/06/assembler/parse.go
+++ projects/06/assembler/parse.go
@@ -1,1 +1,42 @@
 package main
+
+type command string
+
+const (
+	AType  command = "atype"
+	CType  command = "ctype"
+	Ignore command = "ignore"
+)
+
+type parser struct{}
+
+func (p *parser) isLabel(line string) bool {
+	return len(line) > 0 && line[0] == '('
+}
+
+func (p *parser) isComment(line string) bool {
+	return len(line) > 1 && line[0] == '/' && line[1] == '/'
+}
+
+func (p *parser) isAType(line string) bool {
+	return len(line) > 0 && line[0] == '@'
+}
+
+func (p *parser) isCType(line string) bool {
+	return len(line) > 1 && !p.isComment(line) && !p.isLabel(line)
+}
+
+// TODO: Implement parsing fuctions
+func (p *parser) parseLabel(line string) []string { return nil }
+func (p *parser) parseAType(line string) []string { return nil }
+func (p *parser) parseCType(line string) []string { return nil }
+
+func (p *parser) parseLine(line string) (command, []string) {
+	if p.isAType(line) {
+		return AType, p.parseAType(line)
+	} else if p.isCType(line) {
+		return CType, p.parseCType(line)
+	} else {
+		return Ignore, nil
+	}
+}
