20a51ec4fec77263bc85dafd374807f738394ba5
Frederick Santiago <frsantiago2001@gmail.com>
2023-06-30 15:42:17 -0400

    Add codeWriter comments

---
+++ projects/07/vmTranslator/codeWriter.go
@@ -0,0 +1,50 @@
+// This file is in charge of code generation or translating
+// the parsed vm code to its respective asm code. It is
+// broken down into two sections: arithmetic/logical commands
+// and memory access commands.
+//
+// Arithmetic / Locagical commands contain code generation in
+// asm for add, sub, neg, eq, gt, lt, and, or & not.
+//
+// Memory access commands follow the format:
+// 	pop segment i OR push segment i
+// Where a segment can either represent: local, argument,
+// this, that, constant, static, temp, & pointer. These
+// are segments within RAM. In addition, i represents a
+// numerical value to push or pop.
+
+// Arithmetic / Logical Commands
+// add
+
+// sub
+
+// neg
+
+// eq
+
+// gt
+
+// lt
+
+// and
+
+// or
+
+// not
+
+// Memory Access Commands
+// local
+
+// argument
+
+// this
+
+// that
+
+// constant
+
+// static
+
+// temp
+
+// pointer
